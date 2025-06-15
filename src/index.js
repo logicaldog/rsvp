91b1c16e893bb134aea0e06b075fa33b4eb8edd7
logicaldog <sean@fiftyfathom.com>
2025-06-15 11:59:41 -0700

    Add logging to debug form submission error

--- src/index.js
+++ src/index.js
@@ -125,6 +125,7 @@ export default {
         // ✅ Redirect
         return Response.redirect('/thanks.html', 303);
       } catch (err) {
+        
         return jsonError("Something went wrong processing your submission. Please try again.", 500);
       }
     }
